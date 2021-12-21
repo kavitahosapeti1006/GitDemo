f7d84e108bc6a44c1994a86fdb33122a41d1a7e3
kavitahosapeti1006 <kavitahosapeti@gmail.com>
2021-12-21 13:31:36 +0530

    Commiting maven project

---
+++ src/test/java/PracticeMaven/Mavenjava/Practice2Test.java
@@ -0,0 +1,15 @@
+package PracticeMaven.Mavenjava;
+
+
+
+import org.testng.annotations.Test;
+
+public class Practice2Test {
+
+	@Test
+	public void Method2()
+	{
+		System.out.println("Hi");
+		
+	}
+}
---
+++ src/test/java/PracticeMaven/Mavenjava/PracticeTest.java
@@ -0,0 +1,13 @@
+package PracticeMaven.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class PracticeTest {
+	
+	@Test
+	public void mathod1()
+	{
+		System.out.println("Hello");
+	}
+
+}
